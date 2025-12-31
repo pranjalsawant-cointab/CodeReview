e7629294fe198d24f1cb65863be10133acab431b
pranjalsawant-cointab <pranjal.sawant@cointab.net>
2025-12-31 10:39:58 +0530

    Update file7.js

--- file7.js
+++ file7.js
@@ -1,24 +1,5 @@
-function add(a, b) {
-  return a + b
+function greet(name) {
+  return "Hello, " + name;
 }
 
-const result = add(5)
-
-console.log(reslt)
-
-let data = { name: "John" }
-data.age = "25"
-
-if (data.name = "Bob") {
-  console.log("Name changed")
-}
-
-for (i = 0; i < 5; i++) {
-  setTimeout(() => console.log(i), 100)
-}
-function unusedFunction() {
-  // function implementation
-}
-
-unusedFunction();
-
+console.log(greet("World"));
