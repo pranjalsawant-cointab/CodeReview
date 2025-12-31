e9ac5d09e6c798c439996c8289c8812a131496d1
pranjalsawant-cointab <pranjal.sawant@cointab.net>
2025-12-31 10:44:22 +0530

    Update file7.js

--- file7.js
+++ file7.js
@@ -1,5 +1,19 @@
+function escapeHtml(str) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
 function greet(name) {
-  return "Hello, " + name;
+  if (typeof name !== "string" || name.trim() === "") {
+    name = "Guest";
+  }
+
+  const safeName = escapeHtml(name);
+  return "Hello, " + safeName;
 }
 
 console.log(greet("World"));
